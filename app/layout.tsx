f9ba279a693d2bec05a579e67ba9a8af705dc79b
FletchvibezMedia <eric.fletch@gmail.com>
2026-09-16 13:35:14 -0400

    Apply Midnight Mobility preview: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import type {Metadata} from "next";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/barlow-condensed/700.css";
+import "@fontsource/barlow-condensed/800.css";
+import "@fontsource/barlow-condensed/900.css";
 import "./globals.css";
 import "./overrides.css";
 
